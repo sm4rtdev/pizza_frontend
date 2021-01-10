6a351176080e352afb111e97a5afc301d7244eb4
sm4rtdev <elonscottdev@gmail.com>
2021-01-10 16:26:11 +0900

    initial commit of pizza shop

---
+++ src/lib/helper.ts
@@ -0,0 +1,9 @@
+import { OrderItemProps } from "@/providers/order";
+
+
+export function calculateTotalOrder(orders: OrderItemProps[]){
+    return orders.reduce((total, item) =>{
+        const itemTotal = parseFloat(item.product.price) * item.amount;
+        return total + itemTotal
+    }, 0)
+}
